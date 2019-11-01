60c8c43a7703d4ee643cd41c6281756e1c2a92fc
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-11-01 12:29:21 +1100

    Create Host::from()

--- src/host.rs
+++ src/host.rs
@@ -18,8 +18,28 @@ impl Host {
     }
 }
 
+impl From<&str> for Host {
+    /// Creates a Host from a hosts line
+    /// # Example
+    /// ```
+    /// hosts::Host::from("127.0.0.1 localhost");
+    /// ```
+    fn from(s: &str) -> Host {
+        let values: Vec<&str> = s.split_whitespace().collect();
+        Host::new(
+            &values.get(0).expect("IP not found"),
+            &values.get(1).expect("FQDN not found"),
+            match values.get(2..) {
+                Some([]) => None,
+                Some(aliases) => Some(aliases.to_vec().iter().map(|v| v.to_string()).collect()),
+                _ => None,
+            }
+        )
+    }
+}
+
 impl fmt::Display for Host {
-    /// Formats the host to comply with the hosts file standard.
+    /// Formats the host to output the hosts file standard.
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut aliases = String::new();
 
@@ -45,13 +65,19 @@ mod tests {
     }
 
     #[test]
-    fn test_display_with_aliases() {
-        let host = Host::new("127.0.0.1", "localhost", Some(vec!["fennec".to_string()]));
-        assert_eq!(format!("{}", host), "127.0.0.1 localhost fennec");
+    fn test_from() {
+        let host = Host::new("127.0.0.1", "localhost", Some(vec!["test".to_string()]));
+        assert_eq!(Host::from("127.0.0.1 localhost test"), host);
+
+        let host = Host::new("127.0.0.1", "localhost", None);
+        assert_eq!(Host::from("127.0.0.1 localhost"), host)
     }
 
     #[test]
-    fn test_display_without_aliases() {
+    fn test_display() {
+        let host = Host::new("127.0.0.1", "localhost", Some(vec!["test".to_string()]));
+        assert_eq!(format!("{}", host), "127.0.0.1 localhost test");
+
         let host = Host::new("127.0.0.1", "localhost", None);
         assert_eq!(format!("{}", host), "127.0.0.1 localhost");
     }
