ef47efb216fe05c3cf369b43b7c30efe6aa4f601
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-11-06 19:16:17 +1100

    Add example to module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 //! # Hosts
 //!
 //! `hosts` is a collection of tools to parse [hosts](https://en.wikipedia.org/wiki/Hosts_(file)) files.
+//!
+//! # Example
+//! ```
+//! use hosts::{Host, HostsFile};
+//!
+//! // Create a host
+//! let host = Host::new("127.0.0.1", "localhost", None);
+//!
+//! // Create a hosts file
+//! let hosts_file = HostsFile::from(vec!(host));
+//!
+//! // Write the hosts file
+//! hosts_file.write("hosts");
+//! # std::fs::remove_file("hosts");
+//! ```
 
 pub mod host;
 pub mod parser;
